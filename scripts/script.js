807a4ff9cd568c6de5765768ad88941d06a85fd1
mlbrnm <114309689+mlbrnm@users.noreply.github.com>
2024-04-17 10:02:46 -0600

    Update JS to have default duration value of 0.

--- scripts/script.js
+++ scripts/script.js
@@ -11,7 +11,7 @@ You are encouraged to use the provided naming convention for ease of review.
 
 // INSERT YOUR CODE HERE
 let modelName = "XYZ";
-let duration;
+let duration = 0;
 
 /****************** helper function ******************/
 /* create a function called recalculate() which will
